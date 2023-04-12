440fa069bafdd54646d2fbccae45069648088c2e
Наталья Мищенко <mnk96@yandex.ru>
2023-04-12 21:12:22 +0300

    Version 1.0 hw_05

--- yatube/core/models.py
+++ yatube/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class CreatedModel(models.Model):
     created = models.DateTimeField(
         'Дата создания',
@@ -7,4 +8,4 @@ class CreatedModel(models.Model):
     )
 
     class Meta:
-        abstract = True 
+        abstract = True
